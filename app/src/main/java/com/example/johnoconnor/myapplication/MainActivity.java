621d370dce23ba41508736c1af5de98460819eed
gadgettdx <oconnorjohnedward@gmail.com>
2016-08-23 18:06:57 -0400

    Put m/s back in showing up where GeoLocation is... that is so I can help test and determine if we are getting correct speeds.   Put a toast msg that will update with new longitude and latitude every split second it changes.

--- app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.johnoconnor.myapplication;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
+
+    private TextView tvGeoLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         this.onLocationChanged(null);
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onLocationChanged(Location location) {
         TextView tvSpeed = (TextView) this.findViewById(R.id.speed);
+        tvGeoLocation = (TextView) this.findViewById(R.id.geoLocation);
 
         if(location==null){
             tvSpeed.setText("-.- mph");
@@ -31,8 +37,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         else
         {
             double mCurrentSpeed = location.getSpeed();
+            tvGeoLocation.setText(mCurrentSpeed + " m/s");
             mCurrentSpeed = mCurrentSpeed * 2.23694;
             tvSpeed.setText(mCurrentSpeed + " mph");
+
+            String msg = "New Latitude: " + location.getLatitude()
+                   + "New Longitude: " + location.getLongitude();
+
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
         }
     }
 
