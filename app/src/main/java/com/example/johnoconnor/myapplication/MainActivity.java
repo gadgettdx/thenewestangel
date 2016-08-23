8bb48a53dca6a86c0971834e06ccd4c0af17395d
gadgettdx <oconnorjohnedward@gmail.com>
2016-08-23 19:19:21 -0400

    Added a text message feature that when you press the button it will send a text to Cliff and I with your geo location... Dont send too many or try to be funny with it SAM! lol

--- app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
@@ -7,6 +7,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
 
     private TextView tvGeoLocation;
+    private Button btnSendAText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     @Override
-    public void onLocationChanged(Location location) {
+    public void onLocationChanged(final Location location) {
         TextView tvSpeed = (TextView) this.findViewById(R.id.speed);
         tvGeoLocation = (TextView) this.findViewById(R.id.geoLocation);
 
@@ -45,6 +50,30 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                    + "New Longitude: " + location.getLongitude();
 
             Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+            btnSendAText = (Button) findViewById(R.id.btnSendText);
+
+            btnSendAText.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view){
+
+                    String phoneNo = "3367720319";
+                    String phoneNo1 = "7044908662";
+                    String sms = location.getLongitude() + " " + location.getLatitude() + " my location in Charlotte, NC";
+
+                    try {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                        smsManager.sendTextMessage(phoneNo1, null, sms, null, null);
+
+                        Toast.makeText(getApplicationContext(), "SMS Sent!",
+                                Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(),
+                                "SMS faild, please try again later!",
+                                Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
     }
 
