de3a5d7663f9413667ac25607a92f51ff5d9c280
gadgettdx <oconnorjohnedward@gmail.com>
2016-08-23 16:04:32 -0400

    Added GPS function - Not pretty but works - Also went into manifest and asked for Fine Access Location Permition to access GPS signal.  Took the meters per second and converted to MPH.

--- app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.johnoconnor.myapplication;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        this.onLocationChanged(null);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView tvSpeed = (TextView) this.findViewById(R.id.speed);
+
+        if(location==null){
+            tvSpeed.setText("-.- mph");
+        }
+        else
+        {
+            double mCurrentSpeed = location.getSpeed();
+            mCurrentSpeed = mCurrentSpeed * 2.23694;
+            tvSpeed.setText(mCurrentSpeed + " mph");
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
