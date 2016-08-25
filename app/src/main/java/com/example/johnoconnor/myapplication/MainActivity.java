060a9a89ced6a4f837515fdfed2a958a44c92899
gadgettdx <oconnorjohnedward@gmail.com>
2016-08-25 14:52:04 -0400

    Added toolbar but empty at the top and changed color... Need to add menu, app name, and maybe small logo

--- app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/johnoconnor/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
